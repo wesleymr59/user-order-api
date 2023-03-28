92aac7d1662d6e5b51780f970e54da223b118ae0
Wesley <wesleymr59@gmail.com>
2023-03-28 14:33:52 -0300

    Fix: Correção nomes de importação de Classes

--- user-api/src/routers/v1/user_router.py
+++ user-api/src/routers/v1/user_router.py
@@ -6,34 +6,34 @@ from src.schemas.user_schema import userBody, User, userUpdate
 from src.configs.enviroments import get_environment_variables
 
 router = APIRouter(prefix="/v1/users",tags=["user"])
-__user_service = UserService()
-__AuthHandler = AuthHandler()
+user_service = UserService()
+authHandler = AuthHandler()
 env = get_environment_variables()
 
 @router.post("/create")
 async def create_user(body: userBody):
     try:
-        return {"token": __user_service.insert_user(jsonable_encoder(body))}
+        return {"token": user_service.insert_user(jsonable_encoder(body))}
     except:
         raise HTTPException(status_code=400, detail="Não foi possível Criar o usuario")
 
 @router.get("/get_user/{cpf}", response_model=User)
-async def get_user(cpf: str, token = Depends(__AuthHandler.verify_token)) -> (list[tuple] | None):
-    user =  __user_service.get_user(cpf)
+async def get_user(cpf: str, token = Depends(authHandler.verify_token)) -> (list[tuple] | None):
+    user =  user_service.get_user(cpf)
     if user == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Usuario não encontrado")
     return user
 
 @router.delete("/dele_user")
-async def delete_user(cpf: str,token = Depends(__AuthHandler.verify_token)):
+async def delete_user(cpf: str,token = Depends(authHandler.verify_token)):
     try:
-        return __user_service.delete_user(cpf, token)
+        return user_service.delete_user(cpf, token)
     except:
         raise HTTPException(status_code=404, detail="Não foi possível deletar o usuario")
 
 @router.put("/dele_user")
-async def update_user(body: userUpdate, cpf: str, token = Depends(__AuthHandler.verify_token)):
+async def update_user(body: userUpdate, cpf: str, token = Depends(authHandler.verify_token)):
     try:
-        return __user_service.update_user(jsonable_encoder(body), cpf, token)
+        return user_service.update_user(jsonable_encoder(body), cpf, token)
     except:
         raise HTTPException(status_code=404, detail="Não foi possível atualizar o usuario")
--- user-api/src/services/user_service.py
+++ user-api/src/services/user_service.py
@@ -5,42 +5,42 @@ from ..services.auth.tokenAuth import AuthHandler
 from fastapi.encoders import jsonable_encoder
 from ..models.redis.redisdb import SessionRedis
 
-_AuthHandler = AuthHandler()
-_functionsAux = FunctionsAux()
-_user_repository = UserRepository()
-_session = SessionRedis()
+authHandler = AuthHandler()
+functionsAux = FunctionsAux()
+user_repository = UserRepository()
+session = SessionRedis()
+
 class UserService():
     
     def validate_dados(self, body):
-        if not _functionsAux.validar_email(body["email"]):
+        if not functionsAux.validar_email(body["email"]):
             raise HTTPException(status_code=400, detail="Email invalido")
         
         if "cpf" in body:
-            if not _functionsAux.validar_cpf(body["cpf"]):
+            if not functionsAux.validar_cpf(body["cpf"]):
                 raise HTTPException(status_code=400, detail="cpf invalido")
         
-        if not _functionsAux.validar_telefone(body["phone_number"]):
+        if not functionsAux.validar_telefone(body["phone_number"]):
             raise HTTPException(status_code=400, detail="Telefone invalido")
         
     def insert_user(self, body: list):
         self.validate_dados(body)
-        user_created = _user_repository.insert(body)
+        user_created = user_repository.insert(body)
         body["id"]=user_created
-        access_token = _AuthHandler.create_access_token(user_created)
-        _session.addSession(access_token, body)
+        access_token = authHandler.create_access_token(user_created)
+        session.addSession(access_token, body)
         return access_token
     
     def get_user(self, cpf: str):
-        return _user_repository.select(cpf)
+        return user_repository.select(cpf)
         
     def delete_user(self, cpf: str, token: str ):
-            user_session = _session.getSession(token)
-            _user_repository.delete(cpf, user_session["id"])
+            user_session = session.getSession(token)
+            user_repository.delete(cpf, user_session["id"])
             return user_session
         
     def update_user(self, body: list, cpf: str, token:str):
-        
         self.validate_dados(body)
-        user_session = _session.getSession(token)
-        _user_repository.update(body, cpf)
+        user_session = session.getSession(token)
+        user_repository.update(body, cpf)
         return user_session
